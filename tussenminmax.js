0d203244b4b8d304242e731f149976be50e8b1a3
Bssfh16 <boussofhisham@gmail.com>
2025-11-30 19:41:50 +0100

    Update tussenminmax.js
    De tekens kleiner of gelijk aan/groter of gelijk aan waren gevolgd door een getal in plaats van een variabele.
    De getallen werden dus hier veranderd door de variabelen 'Minimum' en 'Maximum'.

--- tussenminmax.js
+++ tussenminmax.js
@@ -13,10 +13,13 @@ let Getal = parseFloat(await userInput.question(('Wat is jouw getal?')));
 let Minimum = 10;
 let Maximum = 20;
 
-if (Getal >= 10 && Getal <= 20){
+if (Getal >= Minimum && Getal <= Maximum){
+    //De tekens kleiner of gelijk aan/groter of gelijk aan waren gevolgd door een getal in plaats van een variabele
+    //De getallen werden dus hier veranderd door de variabelen 'Minimum' en 'Maximum' 
     console.log('Jouw getal ligt tussen '+ Minimum +' en '+ Maximum +'.');
 }else{
     console.log('Jouw getal ligt niet tussen '+ Minimum +' en '+ Maximum +'.');
 }
+
 
 process.exit();
